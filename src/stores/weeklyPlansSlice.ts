f60c6596d1aed0d0f0ec726a915555c2df94863b
Roberto Acan Santizo <prestamotic@legumexadt.net>
2025-01-15 14:42:38 -0600

    feature(creacion plan): agrega creación de plan semanal

--- src/stores/weeklyPlansSlice.ts
+++ src/stores/weeklyPlansSlice.ts
@@ -6,18 +6,28 @@ import { WeeklyPlans } from "../utils/weekly_plans-schema";
 export type WeeklyPlansSliceType = {
     weeklyPlans: WeeklyPlan[];
 
-    loadingFetchPlans: boolean,
+    loadingFetchPlans: boolean;
+    loadingCreatePlan: boolean;
 
     errorFetchPlans: boolean;
+    errorCreatePlan: boolean;
+
+    uploadedFile: File;
 
     fetchPlans: () => Promise<void>;
+    createPlan: (file : File[]) => Promise<void>;
 }
 
 
 export const createWeeklyPlansSlice: StateCreator<WeeklyPlansSliceType> = (set) => ({
     weeklyPlans: [],
     loadingFetchPlans: false,
+    loadingCreatePlan: false,
+
     errorFetchPlans: false,
+    errorCreatePlan: false,
+    uploadedFile: {} as File,
+
     fetchPlans: async () => {
         set({loadingFetchPlans: true});
         try {
@@ -31,6 +41,19 @@ export const createWeeklyPlansSlice: StateCreator<WeeklyPlansSliceType> = (set) 
             set({loadingFetchPlans: false, errorFetchPlans:true});
             throw error;
         }
-    }   
+    },
+    createPlan: async (file) =>{
+        set({loadingCreatePlan: true, uploadedFile: file[0]});
+        try {
+            const url = '/api/plans';
+            const formData = new FormData();
+            formData.append("file", file[0]); 
+    
+            await clienteAxios.post(url,formData);
+            set({loadingCreatePlan: false,errorCreatePlan:false, uploadedFile: {} as File})
+        } catch (error : any) {
+            set({loadingCreatePlan: false,errorCreatePlan:true})
+        }
+    }
 
 })
--- src/views/agricola/planes-semanales/CreatePlanSemanal.tsx
+++ src/views/agricola/planes-semanales/CreatePlanSemanal.tsx
@@ -1,23 +1,44 @@
 import { useCallback, useState } from "react";
 import { useDropzone } from "react-dropzone";
+import { useAppStore } from "../../../stores/useAppStore";
+import { useNavigate } from "react-router-dom";
+
+//COMPONENTES
 import { Button } from "@mui/material";
 import ReturnLink from "../../../components/utilities-components/ReturnLink";
-import Spinner from "../../../components/Spinner";
+import { toast } from "react-toastify";
 
 export default function CreatePlanSemanal() {
-    const [loading, setLoading] = useState(false);
+  const [file, setFile] = useState<File[] | null>(null);
+  const loadingCreatePlan = useAppStore((state) => state.loadingCreatePlan);
+  const uploadedFile = useAppStore((state) => state.uploadedFile);
+  const createPlan = useAppStore((state) => state.createPlan);
+  const navigate = useNavigate();
+
   const onDrop = useCallback((acceptedFiles: File[]) => {
-    setLoading(true);
-    console.log(acceptedFiles);
+    if(acceptedFiles){
+      setFile(acceptedFiles);
+    }
   }, []);
+
   const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop });
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    if (file) {
+      createPlan(file).then(() => {
+        navigate('/planes-semanales');
+        toast.success('Plan Creado Correctamente');
+      });
+    }
+  };
 
   return (
     <>
       <h2 className="text-4xl font-bold">Crear Plan Semanal</h2>
       <ReturnLink url="/planes-semanales" />
 
-      <form className="w-1/2 mx-auto">
+      <form className="w-1/2 mx-auto" onSubmit={handleSubmit}>
         <div
           {...getRootProps()}
           style={{
@@ -25,31 +46,35 @@ export default function CreatePlanSemanal() {
             borderRadius: "10px",
             padding: "20px",
             textAlign: "center",
-            backgroundColor: isDragActive ? "#e3f2fd" : "#f9f9f9", // Cambia el fondo al arrastrar
+            backgroundColor: isDragActive ? "#e3f2fd" : "#f9f9f9",
           }}
         >
-          <input {...getInputProps()} disabled={loading}/>
-          {loading && <Spinner />}
-          {(isDragActive) ? (
-            <p>Suelta el archivo aquí</p>
+          <input
+            {...getInputProps()}
+            disabled={loadingCreatePlan || !!uploadedFile}
+          />
+          {file ? (
+            <p className="text-green-600 font-medium">Archivo: {file[0].name}</p>
+          ) : isDragActive ? (
+            <p className="uppercase font-medium text-blue-500">
+              Suelta el archivo aquí
+            </p>
           ) : (
-            <p className="uppercase font-medium">Arrasta el archivo acá o selecciona</p>
+            <p className="uppercase font-medium text-blue-500">
+              Arrastra el archivo acá
+            </p>
           )}
         </div>
 
         <Button
-          //   disabled={loadingUser}
           type="submit"
           variant="contained"
           color="primary"
           fullWidth
           sx={{ marginTop: 2 }}
+          disabled={!file || loadingCreatePlan}
         >
-          {/* {loadingUser ? ( */}
-          {/* <Spinner /> */}
-          {/* ) : ( */}
           <p className="font-bold text-lg">Crear Plan Semanal</p>
-          {/* )} */}
         </Button>
       </form>
     </>
