023d7bf6f8542b1cebc55d4986db64f4b9d56684
Jose Roberto Acan Santizo <auxtic04@legumexadt.net>
2025-02-11 17:32:26 -0600

    bugfixes

--- src/views/calidad/rmp/IndexRMP.tsx
+++ src/views/calidad/rmp/IndexRMP.tsx
@@ -1,36 +1,7 @@
-import { useEffect, useState } from "react";
-import { toast } from "react-toastify";
 import { PlusIcon } from "lucide-react";
 import { Link } from "react-router-dom";
 
-import { useAppStore } from "../../../stores/useAppStore";
-import Spinner from "../../../components/Spinner";
-
 export default function IndexRMP() {
-    const [loading, setLoading] = useState<boolean>(true);
-    const [error, setError] = useState<boolean>(false);
-    const [boletas, setBoletas] = useState<any[]>([]); // Estado para guardar los datos
-    const getBoletasRMP = useAppStore((state) => state.getBoletasRMP);
-
-    const handleGetInfo = async () => {
-        setLoading(true); // Activar el spinner antes de la petición
-        setError(false);  // Reiniciar el error
-
-        try {
-            const data = await getBoletasRMP(); // Obtener los datos
-            setBoletas(data); // Guardar en el estado
-        } catch (error) {
-            setError(true);
-            toast.error('Hubo un error al traer la información');
-        } finally {
-            setLoading(false); // Desactivar el spinner
-        }
-    };
-
-    useEffect(() => {
-        handleGetInfo();
-    }, []);
-
     return (
         <>
             <div>
@@ -47,27 +18,20 @@ export default function IndexRMP() {
                 </div>
 
                 <div className="p-2 h-96 overflow-y-auto mt-10">
-                    {loading && <Spinner />} 
 
-                     {!loading && error && (
-                        <p className="text-red-500 font-bold">Error al cargar los datos</p>
-                    )}
-
-                    {!loading && !error && (
-                        <table className="table">
-                            <thead>
-                                <tr className="thead-tr">
-                                    <th scope="col" className="thead-th">ID</th>
-                                    <th scope="col" className="thead-th">Placa</th>
-                                    <th scope="col" className="thead-th">Producto</th>
-                                    <th scope="col" className="thead-th">Variedad</th>
-                                    <th scope="col" className="thead-th">Coordinador</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                            </tbody>
-                        </table>
-                    )}
+                    <table className="table">
+                        <thead>
+                            <tr className="thead-tr">
+                                <th scope="col" className="thead-th">ID</th>
+                                <th scope="col" className="thead-th">Placa</th>
+                                <th scope="col" className="thead-th">Producto</th>
+                                <th scope="col" className="thead-th">Variedad</th>
+                                <th scope="col" className="thead-th">Coordinador</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                        </tbody>
+                    </table>
                 </div>
             </div>
         </>
