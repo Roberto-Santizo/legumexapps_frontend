251af4d79e4b2505b7af234246376fc9ec81bf9d
Jose Roberto Acan Santizo <auxtic04@legumexadt.net>
2025-03-13 11:40:51 -0600

    refactor(usuarios react-query): agrega react query a todas las funcionalidades de usuarios

--- src/views/admin/users/EditUser.tsx
+++ src/views/admin/users/EditUser.tsx
@@ -1,85 +1,53 @@
-//HOOKS
 import { useEffect, useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 import { useForm } from "react-hook-form";
-
-//API
 import { updateUser, getUser } from "@/api/UsersAPI";
 import { getRoles } from "@/api/RolesAPI";
 import { getPermissions } from "@/api/PermissionsAPI";
-
-//TYPES
 import { DraftUser, Permission, Role, UserDetail } from "@/types";
-
-//COMPONENTES
 import Spinner from "@/components/Spinner";
 import { Button } from "@mui/material";
 import Error from "@/components/Error";
 import { toast } from "react-toastify";
+import { useQueries,useMutation } from "@tanstack/react-query";
 
 export default function EditUser() {
-  const { id } = useParams();
-  const [loadingGetRoles, setLoadingGetRoles] = useState<boolean>(false);
-  const [loadingGetPermissions, setLoadingGetPermissions] = useState<boolean>(false);
-  const [loadingGetUser, setLoadingGetUser] = useState<boolean>(false);
-  const [loadingUpdateUser, setLoadingUpdateUser] = useState<boolean>(false);
+  const params = useParams();
+  const id = params.id!!;
+  const [selectedPermissions, setSelectedPermissions] = useState<number[]>([]);
+  const navigate = useNavigate();
 
   const [roles, setRoles] = useState<Role[]>([]);
   const [permissions, setPermissions] = useState<Permission[]>([]);
 
   const [userEditing, setUserEditing] = useState<UserDetail>({} as UserDetail);
 
-  const handleGetRoles = async () => {
-    setLoadingGetRoles(true);
-    try {
-      const roles = await getRoles();
-      setRoles(roles);
-    } catch (error) {
-      toast.error(
-        "Hubo un error al traer los roles, intentelo de nuevo más tarde"
-      );
-    } finally {
-      setLoadingGetRoles(false);
-    }
-  };
+  const results = useQueries({
+    queries: [
+      { queryKey: ['getRoles'], queryFn: getRoles },
+      { queryKey: ['getPermissions'], queryFn: getPermissions },
+      { queryKey: ['getUser', id], queryFn: () => getUser(id) }
+    ]
+  });
 
-  const handleGetPermissions = async () => {
-    setLoadingGetPermissions(true);
-    try {
-      const permssions = await getPermissions();
-      setPermissions(permssions);
-    } catch (error) {
-      toast.error(
-        "Hubo un error al traer los permisos, intentelo de nuevo más tarde"
-      );
-    } finally {
-      setLoadingGetPermissions(false);
-    }
-  };
-
-  const handleGetUser = async () => {
-    setLoadingGetUser(true);
-    try {
-      if (id) {
-        const user = await getUser(id);
-        setUserEditing(user);
-      }
-    } catch (error) {
-      toast.error("Hubo un error al traer el usuario, intentelo de nuevo más tarde");
-    } finally {
-      setLoadingGetUser(false);
-    }
-  }
+  const isLoading = results.some(result => result.isFetching);
 
   useEffect(() => {
-    handleGetUser();
-    handleGetPermissions();
-    handleGetRoles();
-  }, []);
+    if (results[0].data) setRoles(results[0].data)
+    if (results[1].data) setPermissions(results[1].data)
+    if (results[2].data) setUserEditing(results[2].data)
+  }, [results])
 
-  const navigate = useNavigate();
-
-  const [selectedPermissions, setSelectedPermissions] = useState<number[]>([]);
+  const {mutate,isPending} = useMutation({
+    mutationFn: ({ id, updatedData }: { id: string; updatedData: DraftUser }) => updateUser(id, updatedData),
+    onError: () => {
+      toast.error('Hubo un error al actualizar el usuario');
+    },
+    onSuccess: () => {
+      toast.success('Usuario actualizado Correctamente');
+      navigate('/usuarios');
+    }
+  });
 
   const {
     register,
@@ -112,195 +80,167 @@ export default function EditUser() {
 
   };
 
-  const editUser = async (data: DraftUser) => {
-    setLoadingUpdateUser(true);
-    const updatedData = { ...data, permissions: selectedPermissions.map(id => ({ id, name: '' })) }
-    if (id) {
-      const errors = await updateUser(id, updatedData);
-      console.log(errors);
-      if (errors) {
-        errors.forEach(error => toast.error(error[0]))
-        setLoadingUpdateUser(false);
-        return;
-      }
-      toast.success("Usuario actualizado correctamente");
-      navigate("/usuarios");
-    }
-    setLoadingUpdateUser(false);
-  };
+  const editUser = async (data: DraftUser) => mutate({id, updatedData: data});
 
+
+  if (isLoading) return <Spinner />
   return (
     <>
-      {!loadingGetUser && userEditing && (
-        <div>
-          <h2 className="text-4xl font-bold">
-            Edición de Usuario: {userEditing.name}
-          </h2>
+      <div>
+        <h2 className="text-4xl font-bold">
+          Edición de Usuario: {userEditing.name}
+        </h2>
+      </div>
+      <form
+        className="mt-10 w-2/3 mx-auto shadow p-10 space-y-5"
+        onSubmit={handleSubmit(editUser)}
+      >
+        <div className="flex flex-col gap-2">
+          <label className="text-lg font-bold uppercase" htmlFor="name">
+            Nombre:
+          </label>
+          <input
+            autoComplete="off"
+            id="name"
+            type="text"
+            placeholder={"Nombre del usuario"}
+            className="border border-black p-3"
+            {...register("name", { required: "El nombre es obligatorio" })}
+          />
+          {errors.name && <Error>{errors.name?.message?.toString()}</Error>}
         </div>
-      )}
 
-      {loadingGetUser && <Spinner />}
-
-      {!loadingGetUser && userEditing && (
-        <form
-          className="mt-10 w-2/3 mx-auto shadow p-10 space-y-5"
-          onSubmit={handleSubmit(editUser)}
-        >
-          <div className="flex flex-col gap-2">
-            <label className="text-lg font-bold uppercase" htmlFor="name">
-              Nombre:
-            </label>
-            <input
-              autoComplete="off"
-              id="name"
-              type="text"
-              placeholder={"Nombre del usuario"}
-              className="border border-black p-3"
-              {...register("name", { required: "El nombre es obligatorio" })}
-            />
-            {errors.name && <Error>{errors.name?.message?.toString()}</Error>}
-          </div>
-
-          <div className="flex flex-col gap-2">
-            <label className="text-lg font-bold uppercase" htmlFor="username">
-              Username:
-            </label>
-            <input
-              autoComplete="off"
-              id="username"
-              type="text"
-              placeholder={"Nombre del usuario"}
-              className="border border-black p-3"
-              {...register("username", {
-                required: "El username es obligatorio",
-                maxLength: {
-                  value: 16,
-                  message: "El username no puede tener más de 16 caracteres",
-                },
-              })}
-            />
-            {errors.username && (
-              <Error>{errors.username?.message?.toString()}</Error>
-            )}
-          </div>
-
-          <div className="flex flex-col gap-2">
-            <label className="text-lg font-bold uppercase" htmlFor="username">
-              Email (Opcional):
-            </label>
-            <input
-              autoComplete="off"
-              id="email"
-              type="email"
-              placeholder={"Correo Electronico"}
-              className="border border-black p-3"
-              {...register("email", {
-                pattern: {
-                  value: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
-                  message: "El formato del email es inválido",
-                },
-              })}
-            />
-            {errors.email && <Error>{errors.email?.message?.toString()}</Error>}
-          </div>
-
-          <div className="flex flex-col gap-2">
-            <label className="text-lg font-bold uppercase" htmlFor="password">
-              Contraseña:
-            </label>
-            <input
-              autoComplete="off"
-              id="password"
-              type="password"
-              placeholder={"Contraseña"}
-              className="border border-black p-3"
-              {...register("password")}
-            />
-            {errors.password && (
-              <Error>{errors.password?.message?.toString()}</Error>
-            )}
-          </div>
-
-          {loadingGetRoles ? (
-            <Spinner />
-          ) : (
-            <div className="flex flex-col gap-2">
-              <label className="text-lg font-bold uppercase" htmlFor="role">
-                Rol:
-              </label>
-
-              <select
-                id="role"
-                className="border border-black p-3"
-                {...register("roles", { required: "El rol es obligatorio" })}
-              >
-                <option value="">--SELECCIONE UNA OPCIÓN--</option>
-                {roles.map((role) => (
-                  <option value={role.name} key={role.id}>
-                    {role.name}
-                  </option>
-                ))}
-              </select>
-
-              {errors.roles && (
-                <Error>{errors.roles?.message?.toString()}</Error>
-              )}
-            </div>
+        <div className="flex flex-col gap-2">
+          <label className="text-lg font-bold uppercase" htmlFor="username">
+            Username:
+          </label>
+          <input
+            autoComplete="off"
+            id="username"
+            type="text"
+            placeholder={"Nombre del usuario"}
+            className="border border-black p-3"
+            {...register("username", {
+              required: "El username es obligatorio",
+              maxLength: {
+                value: 16,
+                message: "El username no puede tener más de 16 caracteres",
+              },
+            })}
+          />
+          {errors.username && (
+            <Error>{errors.username?.message?.toString()}</Error>
           )}
+        </div>
 
-          {loadingGetPermissions ? (
-            <Spinner />
-          ) : (
-            <fieldset className="shadow p-5">
-              <legend className="text-3xl font-bold">Permisos</legend>
-              {errors.permissions && (
-                <Error>{errors.permissions.message?.toString()}</Error>
-              )}
-              <div className="flex flex-col gap-5 mt-5">
-                {permissions.map((permission) => (
-                  <div
-                    className="w-full flex flex-row gap-5 p-5 odd:bg-gray-300 odd:text-white shadow-xl"
-                    key={permission.id}
-                  >
-                    <input
-                      type="checkbox"
-                      id={permission.name}
-                      value={permission.id}
-                      {...register("permissions", {
-                        required: "Selecciona al menos un permiso",
-                      })}
-                      className="w-10"
-                      checked={selectedPermissions.includes(permission.id)}
-                      onChange={() => handleCheckboxChange(permission.id)}
-                    />
-                    <label
-                      className="font-bold text-xl"
-                      htmlFor={permission.name}
-                    >
-                      {permission.name}
-                    </label>
-                  </div>
-                ))}
-              </div>
-            </fieldset>
+        <div className="flex flex-col gap-2">
+          <label className="text-lg font-bold uppercase" htmlFor="username">
+            Email (Opcional):
+          </label>
+          <input
+            autoComplete="off"
+            id="email"
+            type="email"
+            placeholder={"Correo Electronico"}
+            className="border border-black p-3"
+            {...register("email", {
+              pattern: {
+                value: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
+                message: "El formato del email es inválido",
+              },
+            })}
+          />
+          {errors.email && <Error>{errors.email?.message?.toString()}</Error>}
+        </div>
+
+        <div className="flex flex-col gap-2">
+          <label className="text-lg font-bold uppercase" htmlFor="password">
+            Contraseña:
+          </label>
+          <input
+            autoComplete="off"
+            id="password"
+            type="password"
+            placeholder={"Contraseña"}
+            className="border border-black p-3"
+            {...register("password")}
+          />
+          {errors.password && (
+            <Error>{errors.password?.message?.toString()}</Error>
           )}
+        </div>
 
-          <Button
-            disabled={loadingUpdateUser}
-            type="submit"
-            variant="contained"
-            color="primary"
-            fullWidth
-            sx={{ marginTop: 2 }}
+        <div className="flex flex-col gap-2">
+          <label className="text-lg font-bold uppercase" htmlFor="role">
+            Rol:
+          </label>
+
+          <select
+            id="role"
+            className="border border-black p-3"
+            {...register("roles", { required: "El rol es obligatorio" })}
           >
-            {loadingUpdateUser ? (
-              <Spinner />
-            ) : (
-              <p className="font-bold text-lg">Actualizar Usuario</p>
-            )}
-          </Button>
-        </form>
-      )}
+            <option value="">--SELECCIONE UNA OPCIÓN--</option>
+            {roles.map((role) => (
+              <option value={role.name} key={role.id}>
+                {role.name}
+              </option>
+            ))}
+          </select>
+
+          {errors.roles && (
+            <Error>{errors.roles?.message?.toString()}</Error>
+          )}
+        </div>
+
+        <fieldset className="shadow p-5">
+          <legend className="text-3xl font-bold">Permisos</legend>
+          {errors.permissions && (
+            <Error>{errors.permissions.message?.toString()}</Error>
+          )}
+          <div className="flex flex-col gap-5 mt-5">
+            {permissions.map((permission) => (
+              <div
+                className="w-full flex flex-row gap-5 p-5 odd:bg-gray-300 odd:text-white shadow-xl"
+                key={permission.id}
+              >
+                <input
+                  type="checkbox"
+                  id={permission.name}
+                  value={permission.id}
+                  {...register("permissions", {
+                    required: "Selecciona al menos un permiso",
+                  })}
+                  className="w-10"
+                  checked={selectedPermissions.includes(permission.id)}
+                  onChange={() => handleCheckboxChange(permission.id)}
+                />
+                <label
+                  className="font-bold text-xl"
+                  htmlFor={permission.name}
+                >
+                  {permission.name}
+                </label>
+              </div>
+            ))}
+          </div>
+        </fieldset>
+
+        <Button
+          disabled={isPending}
+          type="submit"
+          variant="contained"
+          color="primary"
+          fullWidth
+          sx={{ marginTop: 2 }}
+        >
+          {isPending ? (
+            <Spinner />
+          ) : (
+            <p className="font-bold text-lg">Actualizar Usuario</p>
+          )}
+        </Button>
+      </form>
     </>
   );
 }
