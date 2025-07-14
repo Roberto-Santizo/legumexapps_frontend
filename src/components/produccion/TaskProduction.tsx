a2fc57f9922a7f5460b65400ff575a42ee237769
Roberto Santizo <soportetecnico.tejar@legumex.net>
2025-07-14 08:29:48 -0600

    preventivo

--- src/components/produccion/TaskProduction.tsx
+++ src/components/produccion/TaskProduction.tsx
@@ -92,12 +92,12 @@ export default function TaskProduction({ task, isFetching }: Props) {
               </button>
             )}
 
-            {task.status === 3 && (
+            {/* {task.status === 3 && ( */}
               <SquarePlay
                 onClick={() => startTask(task.id)}
                 className="w-6 h-6 text-gray-700 hover:text-gray-500 cursor-pointer transition"
               />
-            )}
+            {/* )} */}
 
             {task.status === 4 && !task.paused && (
               <>
