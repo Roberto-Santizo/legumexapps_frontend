5794411f478181593109b7a415b665025f0ef723
Roberto Santizo <soportetecnico.tejar@legumex.net>
2025-10-16 16:02:03 -0600

    bugfix

--- src/views/agricola/cdps/Index.tsx
+++ src/views/agricola/cdps/Index.tsx
@@ -1,5 +1,3 @@
-import { PlusIcon } from "lucide-react";
-import { Link } from "react-router-dom";
 import { useEffect, useState } from "react";
 import { getCDPS } from "@/api/PlantationControlAPI";
 import { useQuery } from "@tanstack/react-query";
